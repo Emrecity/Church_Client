71788aea7508d6915bd9af511d495e3241fd4de0
emrecity <owusu2020emmanuel@gmail.com>
2024-11-30 13:56:03 +0000

    fixed event page

--- src/Events.jsx
+++ src/Events.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Events = () => {
+    const navigate = useNavigate()
   return (
     <div className='w-screen min-h-screen my-[15%] text-center text-blue-500'>
-        Events Page under construction
+        <h1>Events Page under construction</h1>
         <button onClick={()=>navigate(-1)} className='p-1 px-5 my-3 text-white bg-blue-500 rounded'>Back</button>
     </div>
   )
